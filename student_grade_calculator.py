5ba48509c3e71a19a435749b7a2c03626543df79
SWARALI SURYAWANSHI <swaralisuryawanshi123@gmail.com>
2026-03-06 02:25:02 +0530

    Create student_grade_calculator.py

---
+++ student_grade_calculator.py
@@ -0,0 +1,18 @@
+print("Student Grade Calculator")
+
+m1 = float(input("Enter marks for Subject 1: "))
+m2 = float(input("Enter marks for Subject 2: "))
+m3 = float(input("Enter marks for Subject 3: "))
+
+average = (m1 + m2 + m3) / 3
+
+print("Average Marks:", average)
+
+if average >= 90:
+    print("Grade: A")
+elif average >= 75:
+    print("Grade: B")
+elif average >= 50:
+    print("Grade: C")
+else:
+    print("Grade: Fail")
